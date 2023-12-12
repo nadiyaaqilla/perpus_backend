f7de6b564f543ec70e5255bef72d29637b239819
yyoosshhh <juliyosiyos@gmail.com>
2023-12-12 21:47:39 +0700

    Halaman Produk Admin

---
+++ produk_admin.php
@@ -0,0 +1,42 @@
+<?php
+$respon = [
+    "status" => 200,
+    "msg" => "",
+    "body" => [
+        "data" => []
+    ]
+];
+
+$koneksi = mysqli_connect("localhost", "root", "", "e-perpus");
+
+if (isset($_POST['submit'])) {
+    $judul = $_POST['judul_bk'];
+    $isbn = $_POST['isbn_bk'];
+    $kategori = $_POST['nama_kategori'];
+    $sinopsis = $_POST['sinop_bk'];
+    $cover = $_FILES['cover_bk'];
+    $coverDir = "../file/img/";
+    $targetcover = $coverDir . basename($_FILES["cover_bk"]["name"]);
+    move_uploaded_file($_FILES["cover_bk"]["tmp_name"], $targetcover);
+    $file_bk = $_FILES['file_bk'];
+    $fileDir = "../file/";
+    $targetFile = $fileDir . basename($_FILES["file_bk"]["name"]);
+    move_uploaded_file($_FILES["file_bk"]["tmp_name"], $targetFile);
+
+    $sql = "INSERT INTO data_buku (cover_bk, judul_bk, isbn_bk, nama_kategori, sinop_bk, file_bk) 
+        VALUES ('$targetcover', '$judul', '$isbn', '$kategori', '$sinopsis', '$targetFile')";
+
+    if ($koneksi->query($sql) === TRUE) {
+        $respon["status"] = "success";
+        $respon["msg"] = "Data berhasil ditambahkan";
+    } else {
+        $respon["status"] = "error";
+        $respon["msg"] = "Error: " . $sql . "<br>" . $koneksi->error;
+    }
+}
+
+// Always output JSON, whether success or error
+header('Content-Type: application/json');
+echo json_encode($respon);
+
+$koneksi->close();
